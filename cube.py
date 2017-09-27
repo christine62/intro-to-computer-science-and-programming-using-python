e98821c429613d24f950df5d8aa36c5bc69aab78
christine62 <32208483+christine62@users.noreply.github.com>
2017-09-27 18:44:57 -0400

    Add files via upload

---
+++ cube.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 10 12:03:32 2017
+
+@author: jiahuibi
+"""
+
+cube=27
+epsilon = 0.01
+guess = 0.0
+increment = 0.0001
+num_guesses = 0
+while abs(guess**3-cube)>=epsilon and guess <=cube:
+    guess += increment
+    num_guesses += 1
+print("num_guesses =", num_guesses)
+if abs(guess**3-cube)>=epsilon:
+    print("Failed on cube root of", cube)
+else:
+    print(guess,'is close to the cube root of', cube)
