d149f69f371e8c93a4a4438897dccd0218a490d3
christine62 <32208483+christine62@users.noreply.github.com>
2017-09-29 11:59:42 -0400

    Create iter&recurgcd.py

---
+++ iter&recurgcd.py
@@ -0,0 +1,26 @@
+def gcdIter(a, b):
+    '''
+    a, b: positive integers
+    
+    returns: a positive integer, the greatest common divisor of a & b.
+    '''
+    # Your code here
+    c = min(a,b)
+    while c>0:
+        if (a%c)==0 and (b%c) ==0:
+            return c
+        else:
+            c -= 1
+           
+ def gcdRecur(a, b):
+    '''
+    a, b: positive integers
+    
+    returns: a positive integer, the greatest common divisor of a & b.
+    '''
+    # Base case is when b = 0
+    if b == 0:
+        return a
+
+    # Recursive case
+    return gcdRecur(b, a % b)
